8e94711bb16d8f65cf67ab62efdf1ce3a8240bec
ptjohns2 <ptjohns2@asu.edu>
2014-07-24 11:39:09 -0700

    Refresh

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int main(){
 	//string programName = "macrotest.txt";
 	//string programName = "recursiontest.txt";
 	//tring programName = "prime-1.txt";
-	//string programName = "prime-2.s";
+	//string programName = "prime-2.s";a
 	//string programName = "fact.s";
 	//string programName = "testProgram2.txt";
 	assembler.loadProgramFromFile(programName);
